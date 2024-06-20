801df7af9885376b485ad29a72111b81178ecb5e
Justin <justin.wang4@icloud.com>
2024-06-20 10:20:28 +0800

    [add] preparation

--- GitFeed-RxSwift/ActivityController.swift
+++ GitFeed-RxSwift/ActivityController.swift
@@ -1,17 +1,17 @@
 /// Copyright (c) 2024
 /// GitFeed-RxSwift
-/// JustinWang
-///
+/// JustinWang    
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -19,7 +19,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 /// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 /// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -28,15 +28,63 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
+import Foundation
 import UIKit
+import RxSwift
+import RxRelay
+import RxCocoa
+import Kingfisher
 
-class ViewController: UIViewController {
-
+class ActivityController: UITableViewController {
+  private let repo = "ReactiveX/RxSwift"
+  
+  private let events = BehaviorRelay<[Event]>(value: [])
+  private let bag = DisposeBag()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    title = repo
+    
+    self.refreshControl = UIRefreshControl()
+    let refreshControl = self.refreshControl!
+    
+    refreshControl.backgroundColor = UIColor(white: 0.98, alpha: 1.0)
+    refreshControl.tintColor = UIColor.darkGray
+    refreshControl.attributedTitle = NSAttributedString(string: "Pull to Refresh")
+    refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+    
+    refresh()
   }
-
-
+  
+  @objc func refresh() {
+    DispatchQueue.global(qos: .default).async { [weak self] in
+      guard let self = self else { return }
+      self.fetchEvents(repo: self.repo)
+    }
+  }
+  
+  func fetchEvents(repo: String) {
+    
+  }
+  
+  func processEvents(_ newEvents: [Event]) {
+    
+  }
+  
+  // MARK: - Table Data Source
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return events.value.count
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let event = events.value[indexPath.row]
+    
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+    cell.textLabel?.text = event.actor.name
+    cell.detailTextLabel?.text = event.repo.name + ", " + event.action.replacingOccurrences(of: "Event", with: "").lowercased()
+    cell.imageView?.kf.setImage(with: event.actor.avatar, placeholder: UIImage(named: "blank-avatar"))
+    return cell
+  }
+  
 }
-
